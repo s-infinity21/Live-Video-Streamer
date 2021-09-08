fb081c09d64fe072655513bcef1e5337c3cf1c3a
Shubh Chaurasia <83367007+s-infinity21@users.noreply.github.com>
2021-09-08 14:40:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask,render_template,Response
 import cv2
 
 app=Flask(__name__)
+
+# Capture Video using OpenCV
 camera=cv2.VideoCapture(0) # for PC webcam only
 # for cctv camera use 'rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp'
 
+# Adding window and generating frames from webcam/cctv
 def genFrames():
     while True:
             
@@ -16,14 +19,14 @@ def genFrames():
             frame=buffer.tobytes()
 
         yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') # concatenate frames and yields result
 
 
-@app.route('/')
+@app.route('/') # default route
 def index():
     return render_template('index.html')
 
-@app.route('/live')
+@app.route('/live') # The ‘/live’ route returns the streaming response
 def liveVideo():
     return Response(genFrames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
